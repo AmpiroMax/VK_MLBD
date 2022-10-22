baa31055a0007dfb559d85d277e74b68e4623bf0
AmpiroMax <patrat2015@yandex.ru>
2022-10-22 23:28:23 +0300

    Scripts are working

--- mapper_mean.py
+++ mapper_mean.py
@@ -1,23 +1,26 @@
-#!/usr/bin/python3
 import sys
-
+import csv
 
 def mapper():
     PRICE_COLUMN = 9
-    
     chunk_count = 0
     total_sum = 0
 
-    for line in sys.stdin:
-        price = line.split(',')[PRICE_COLUMN]
-        if not price.isdigit():
+    for line in csv.reader(sys.stdin, delimiter=",")
+        price = line[PRICE_COLUMN]
+        
+        try:
+            price = int(price)
+        except ValueError:
             continue
-        price = int(price)
+
         chunk_count += 1
         total_sum += price
 
-    chunk_mean = total_sum / chunk_count
-
+    if chunk_count == 0:
+        chunk_mean = 0
+    else:
+        chunk_mean = total_sum / chunk_count
     print(chunk_count, chunk_mean)
 
 if __name__ == "__main__":
--- mapper_variance.py
+++ mapper_variance.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
 import sys
-
+import csv
 
 def mapper():
     PRICE_COLUMN = 9
@@ -9,16 +8,20 @@ def mapper():
     total_sum = 0
     total_sq_sum = 0
 
-    for line in sys.stdin:
-        price = line.split(',')[PRICE_COLUMN]
-        if not price.isdigit():
+    for line in csv.reader(sys.stdin, delimiter=",")
+        price = line[PRICE_COLUMN]
+
+        try:
+            price = int(price)
+        except ValueError:
             continue
+
         chunk_count += 1
         total_sum += price
         total_sq_sum += price**2
 
     chunk_mean = total_sum / chunk_count
-    chunk_variance = total_sq_sum / chunk_count - chunk_mean
+    chunk_variance = total_sq_sum / chunk_count - chunk_mean**2
     print(chunk_count, chunk_mean, chunk_variance)
 
 if __name__ == "__main__":
